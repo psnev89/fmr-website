a5a676ed9efc4e11c9c9b6f3b6bbb82dde927a6c
Pedro Neves <pedro.neves@unbabel.com>
2025-07-30 16:11:38 +0100

    Add Netlify Identity configuration files and update layout reference in homepage

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,13 @@
-export default function () {
+export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({
+        "./src/admin/config.yml": "./admin/config.yml",
+    });
     return {
         dir: {
             input: "src",
+            includes: "_includes",
+            data: "_data",
+            layouts: "_includes/layouts",
             output: "_site",
         },
     };
