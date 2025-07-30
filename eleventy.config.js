453bd95c8b4a8772fe20e431ac5746f63c02f4c6
Pedro Neves <pedro.neves@unbabel.com>
2025-07-30 20:22:37 +0100

    Add logo image and refactor navbar structure for improved layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
         "./src/admin/config.yml": "./admin/config.yml",
+        "./src/assets/images": "./assets/images",
     });
 
     return {
